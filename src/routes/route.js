dc84b642f97f055981b4b5a2ea47b67aebd70d41
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-06-25 09:17:13 +0500

    first commit

---
+++ src/routes/route.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+
+const scrollBehavior = () => {
+    return {top:0}
+};
+
+const routes = [
+    {
+        path: "/",
+        name: "home",   
+        component: () => import("../pages/Home.vue"),   
+    },
+   
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    scrollBehavior,
+});
+
+export default router;
