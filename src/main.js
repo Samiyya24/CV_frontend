dc4851ceffe37d856260558bdd469b40803aa285
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-07-01 08:50:42 +0500

    updated

--- src/main.js
+++ src/main.js
@@ -2,15 +2,12 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
-import i18n from "./plugins/i18n";
 import router from "./routes/route";
 
 const app = createApp(App);
 const pinia = createPinia();
 
-app.config.globalProperties.$test = "test";
 
-app.use(i18n);
 app.use(pinia);
 app.use(router);
 app.mount("#app");
